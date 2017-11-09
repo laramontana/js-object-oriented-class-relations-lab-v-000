04025993903af0ce274146e64bcabe7586c79e73
laramontana <larabrightside@gmail.com>
2017-11-09 12:03:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 let store = {drivers: [], trips: [], passengers: []};
-let driverId = 0;
-let tripId = 0;
-let passengerId = 0;
 
+let driverId = 0;
 
 class Driver {
   constructor(name) {
@@ -10,19 +8,62 @@ class Driver {
     this.name = name;
     store.drivers.push(this);
   }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    })
+  }
+
+  passengers(){
+    return this.trips().map((trip)=> {
+      return trip.passenger()
+    })
+  }
+
 }
+
+let passengerId = 0;
 
 class Passenger {
   constructor(name) {
-    this.id = ++driverId;
+    this.id = ++passengerId;
     this.name = name;
     store.passengers.push(this);
   }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id;
+    })
+  }
+
+  drivers(){
+    return this.trips().map((trip)=> {
+      return trip.driver()
+    })
+  }
+
 }
 
+let tripId = 0;
+
 class Trip {
-  constructor(passenger, driver) {
+  constructor(driver, passenger) {
     this.id = ++tripId;
     store.trips.push(this);
+    if (driver && passenger) {
+
+     this.passengerId = passenger.id;
+     this.driverId = driver.id;
+   }
+  }
+
+  driver() {
+    return store.drivers.find(driver => {return driver.id == this.driverId});
+  }
+
+  passenger() {
+    return store.passengers.find(passenger => {return passenger.id == this.passengerId});
   }
 }
